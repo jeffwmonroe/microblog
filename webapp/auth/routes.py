d120d94b744fa2bc0c8895f28666a77452f1a30a
Jeff Monroe <jeff@jeff-monroe.com>
2023-02-26 16:44:53 -0800

    Simple Cleanup
    Moved logout to have a login_required decorator

--- webapp/auth/routes.py
+++ webapp/auth/routes.py
@@ -29,8 +29,9 @@ def login():
     return render_template('login.html', title='Sign In', form=form)
 
 
-# @login_required
+
 @bp.route('/logout')
+@login_required
 def logout():
     logout_user()
     return redirect(url_for('auth.login'))
